94425c731e3d3670d9febb47aba8ba6723878d51
DimaTivator <andriianov164@gmail.com>
2023-12-31 16:58:36 +0300

    to_numpy transformation

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
 import numpy as np
 import math
 
+import pandas as pd
 
 INF = float('inf')
+
+
+def to_numpy(obj):
+    if type(obj) is pd.DataFrame:
+        return obj.values
+
+    if type(obj) is pd.Series:
+        return np.reshape(obj.values, (len(obj.values), 1))
+
+    if type(obj) is list:
+        return np.array(obj)
+
 
