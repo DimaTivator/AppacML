5162fc7f7ec0260ce99ac53f2bf88f83a586612e
DimaTivator <andriianov164@gmail.com>
2024-01-03 00:32:43 +0300

    Node class update. Id added. __str__ magic method added.

--- tree/tree.py
+++ tree/tree.py
@@ -1,3 +1,6 @@
+from utils import *
+
+
 class Node:
     def __init__(
             self,
@@ -5,17 +8,29 @@ class Node:
             y,
             depth,
             sep_feature,
-            sep_value,
-            is_leaf=False
+            value,
+            is_leaf=False,
+            ID=0
     ):
-        self.is_leaf = is_leaf
-        self.depth = depth
-        self.children = []
-        self.sep_feature = sep_feature
-        self.sep_value = sep_value
         self.X = X
         self.y = y
+        self.depth = depth
+        self.sep_feature = sep_feature
+        self.value = value
+        self.is_leaf = is_leaf
+        self.children = []
+        self.__id = ID
 
+    @property
+    def ID(self):
+        return self.__id
 
-if __name__ == "__main__":
-    node = Node()
+    def __str__(self):
+        return (
+                f'NODE ID: {self.__id}\n'
+                f'Depth: {self.depth}\n'
+                f'Value {self.value}\n'
+                f'Sep by: feature {self.sep_feature}\n'
+                f'Data: {np.concatenate((self.X, self.y), axis=1)}\n'
+                )
+
