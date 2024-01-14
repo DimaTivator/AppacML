7b2f94ff53625fdd6cc5c404ea585551fc6baad7
DimaTivator <andriianov164@gmail.com>
2024-01-14 13:28:28 +0300

    NEW implemented _get_init_attributes_names method

--- base/BaseEstimator.py
+++ base/BaseEstimator.py
@@ -1,3 +1,4 @@
+import inspect
 from abc import ABC, abstractmethod
 
 
@@ -5,6 +6,21 @@ class BaseEstimator(ABC):
 
     def _get_param_names(self):
         pass
+    @classmethod
+    def _get_init_attributes_names(cls):
+
+        """
+        Checking if the given class provides __init__ method implementation
+        In this case we'll take the attributes from its signature
+        Otherwise we'll look fot the attribute names in cls.__dict__
+        """
+
+        if hasattr(cls, '__init__'):
+            init_signature = inspect.signature(cls.__init__)
+            return [param for param in init_signature.parameters.keys() if param != 'self']
+
+        return [attr for attr in cls.__dict__ if not callable(getattr(cls, attr)) and not attr.startswith("__")]
+
 
     def get_params(self, deep=True):
         pass
