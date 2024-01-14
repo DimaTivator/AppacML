ac309bbd413006209afd1b20b09893d50313c337
DimaTivator <andriianov164@gmail.com>
2024-01-14 13:32:42 +0300

    NEW implemented get_params method

--- base/BaseEstimator.py
+++ base/BaseEstimator.py
@@ -4,8 +4,6 @@ from abc import ABC, abstractmethod
 
 class BaseEstimator(ABC):
 
-    def _get_param_names(self):
-        pass
     @classmethod
     def _get_init_attributes_names(cls):
 
@@ -21,9 +19,16 @@ class BaseEstimator(ABC):
 
         return [attr for attr in cls.__dict__ if not callable(getattr(cls, attr)) and not attr.startswith("__")]
 
+    def get_params(self, **kwargs):
 
-    def get_params(self, deep=True):
-        pass
+        """
+        Gets estimator's parameters
+
+        ! It's assumed that all parameters are defined in __init__ method signature
+        """
+
+        attribute_names = self._get_init_attributes_names()
+        return {name: getattr(self, name) for name in attribute_names}
 
     @abstractmethod
     def fit(self, X, y):
