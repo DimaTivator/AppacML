f9e1c4bf88225607efdb3581f42b1adea3fa309f
DimaTivator <andriianov164@gmail.com>
2024-01-17 23:42:22 +0300

    NEW: add precision_score

--- metrics/_classification.py
+++ metrics/_classification.py
@@ -10,3 +10,10 @@ def accuracy_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     shape = list(y_true.shape)
     total = ([shape[0]] + [shape[i] * shape[i - 1] for i in range(1, len(shape))])[-1]
     return correct / total
+
+
+# What part of retrieved items is relevant
+def precision_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    true_positives = np.sum(y_true * y_pred)    # the number of cases where 1 was correctly predicted
+    false_positives = np.sum(~y_true * y_pred)  # the number of cases where 1 was incorrectly predicted
+    return true_positives / (true_positives + false_positives)
